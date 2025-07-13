0ebe01b714547bcb7ae8d01e0c128da6121929e4
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-07-13 14:37:05 +0530

    Time: 4 ms (55.57%) | Memory: 17.8 MB (84.97%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -8,7 +8,7 @@ class Solution:
       dummyhead = ListNode(0)
       curr = dummyhead
       carry = 0
-      while l1!= None or l2 != None or carry != 0:
+      while l1 or l2  or carry:
         l1Val = l1.val if l1 else 0
         l2Val = l2.val if l2 else 0
         columnSum = l1Val + l2Val + carry
