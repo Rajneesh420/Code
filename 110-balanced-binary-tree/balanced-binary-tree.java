b6dc8dd9d54258c6626c60db77e37fb740805bb8
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-08-14 00:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (7.99%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,31 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int depth(TreeNode root) {
-        if(root == null) return 0;
-        int left = depth(root.left);
-        int right = depth(root.right);
-        return Math.max(left,right)+1;
-    }
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        int left = depth(root.left);
-        int right = depth(root.right);
-        if(Math.abs(left-right)>1) return false;
-        return isBalanced(root.left) && isBalanced(root.right);
-        
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = height(node.left);
+        if (left == -1) return -1; // left subtree not balanced
+
+        int right = height(node.right);
+        if (right == -1) return -1; // right subtree not balanced
+
+        if (Math.abs(left - right) > 1) return -1; // current node not balanced
+
+        return Math.max(left, right) + 1;
     }
 }
