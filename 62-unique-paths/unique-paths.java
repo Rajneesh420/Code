6abc22ac9f91098b14508062610fe7882e9a332c
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-08 00:52:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (77.87%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -3,8 +3,7 @@ class Solution {
         if(i>=m || j>=n) return 0;
         if(i==m-1 && j==n-1) return 1;
         if(dp[i][j]!=0) return dp[i][j];
-        int count = DFS(i+1,j,m,n,dp)+DFS(i,j+1,m,n,dp);
-        dp[i][j] =count;
+        dp[i][j]= DFS(i+1,j,m,n,dp)+DFS(i,j+1,m,n,dp);
         return dp[i][j];
     }
     public int uniquePaths(int m, int n) {
