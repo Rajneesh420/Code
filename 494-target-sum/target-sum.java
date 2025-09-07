2e14b6f74296ab079604f5b0d0d44d9024ecc1ab
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-08 01:25:19 +0530

    Time: 7 ms (66.72%) | Memory: 45 MB (26.18%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,14 +1,30 @@
 class Solution {
-    public int DFS(int[] nums, int sum, int i, int target) {
-        if(i == nums.length) {
+    public int DFS(int[] nums, int sum, int i, int target,int[][] dp,int offset) {
+        if (i == nums.length) {
             return sum == target ? 1 : 0;
         }
-        int count1 = DFS(nums, sum + nums[i], i + 1, target);
-        int count2 = DFS(nums, sum - nums[i], i + 1, target);
-        return count1 + count2;
+        
+        if (dp[i][sum + offset] != -1) {
+            return dp[i][sum + offset];
+        }
+        
+        int count1 = DFS(nums, sum + nums[i], i + 1, target, dp, offset);
+        int count2 = DFS(nums, sum - nums[i], i + 1, target, dp, offset);
+        
+        dp[i][sum + offset] = count1 + count2;
+        return dp[i][sum + offset];
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-        return DFS(nums, 0, 0, target);
+        int n = nums.length;       
+        int offset = 0;
+        for (int num : nums) {
+            offset += num;
+        }       
+        int[][] dp = new int[n][2 * offset + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }   
+        return DFS(nums, 0, 0, target, dp, offset);
     }
 }
