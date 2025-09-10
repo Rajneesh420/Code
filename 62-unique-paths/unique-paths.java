431fe4cacf3f79f2ad37799a153b24a8f887d7ca
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-10 13:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (61.77%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,13 +1,19 @@
 class Solution {
-    public static int DFS(int i,int j,int m,int n,int[][] dp) {
-        if(i>=m || j>=n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=0) return dp[i][j];
-        dp[i][j]= DFS(i+1,j,m,n,dp)+DFS(i,j+1,m,n,dp);
-        return dp[i][j];
-    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        return DFS(0,0,m,n,dp);
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j== 0)
+                {
+                    dp[i][j] = 1;
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
