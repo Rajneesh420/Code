2b22689bd8fea67b399302cc3ca48c2f79184fed
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-11 15:46:21 +0530

    Time: 10 ms (64.94%) | Memory: 44.4 MB (98.81%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26];  
+        int maxCount = 0;  
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int idx = s.charAt(right) - 'A';
+            count[idx]++;
+            maxCount = Math.max(maxCount, count[idx]);
+
+            if (right - left + 1 - maxCount > k) {
+                count[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
