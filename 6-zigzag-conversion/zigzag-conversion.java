c190c664de0f678851420ed1c7b71e27f6841d02
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-12 12:05:52 +0530

    Time: 6 ms (32.95%) | Memory: 45.2 MB (47.69%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,19 +1,24 @@
 class Solution {
-    public String convert(String s, int nRows) {
-    char[] c = s.toCharArray();
-    int len = c.length;
-    StringBuffer[] sb = new StringBuffer[nRows];
-    for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
-    
-    int i = 0;
-    while (i < len) {
-        for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
-            sb[idx].append(c[i++]);
-        for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
-            sb[idx].append(c[i++]);
+    public String convert(String s, int numRows) {
+        if(numRows == 1 || numRows >= s.length()) return s;
+        int idx = 0,d=1;
+        List<Character>[] rows = new ArrayList[numRows];
+        for(int i=0;i<numRows;i++) rows[i] = new ArrayList<>();
+
+        for(char c : s.toCharArray()) {
+            rows[idx].add(c);
+            if(idx == 0)
+            d = 1;
+            else if(idx == numRows-1)
+            d = -1;
+            idx +=d;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(List<Character> row : rows){
+            for(char c: row)
+            result.append(c);
+        }
+        return result.toString();
     }
-    for (int idx = 1; idx < sb.length; idx++)
-        sb[0].append(sb[idx]);
-    return sb[0].toString();
-}
 }
