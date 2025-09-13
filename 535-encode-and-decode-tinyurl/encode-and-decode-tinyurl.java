e3bb661938a1f4a05ea0c087c737a6b861188e67
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-13 21:20:52 +0530

    Time: 2 ms (70.41%) | Memory: 43 MB (69.66%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
@@ -1,24 +1,18 @@
 public class Codec {
-    Map<String, String> codeDB = new HashMap<>(), urlDB = new HashMap<>();
-    static final String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private Map<String, String> map = new HashMap<>();
+    private static final String BASE_URL = "http://tinyurl.com/";
+    private int counter = 1;
 
-    private String getCode() {
-        char[] code = new char[6];
-        for (int i = 0; i < 6; i++) 
-            code[i] = chars.charAt((int)(Math.random() * 62));
-        return "http://tinyurl.com/" + String.valueOf(code);
-    }
-    
+    // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        if (urlDB.containsKey(longUrl)) return urlDB.get(longUrl);
-        String code = getCode();
-        while (codeDB.containsKey(code)) code = getCode();
-        codeDB.put(code, longUrl);
-        urlDB.put(longUrl, code);
-        return code;
+        String key = String.valueOf(counter++);
+        String shortUrl = BASE_URL + key;
+        map.put(shortUrl, longUrl);
+        return shortUrl;
     }
 
+    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return codeDB.get(shortUrl);
+        return map.get(shortUrl);
     }
 }
