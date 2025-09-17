366835c268c0e4f2428de562d3c05646f1cc1f58
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-17 13:44:38 +0530

    Time: 3 ms (56.29%) | Memory: 44.4 MB (82.33%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -15,21 +15,24 @@
  */
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
-         Deque<TreeNode> deq = new LinkedList<>();
-        deq.addLast(root);
-        List<List<Integer>> result = new ArrayList<>();
-        if(root == null) return 0;
-        while(!deq.isEmpty()) {
-            int n = deq.size();
-            List<Integer> local = new ArrayList<>();
-            for(int i=0;i<n;i++){
-            TreeNode t = deq.pollFirst();
-            local.add(t.val);
-            if(t.left != null) deq.addLast(t.left);
-            if(t.right != null) deq.addLast(t.right);
+        if(root == null ) return -1;
+        Deque<TreeNode> q = new LinkedList<>();
+        q.addLast(root);
+        int ans = -1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+
+            for(int i =0; i<size ; i++){
+            TreeNode current = q.pollFirst();
+            if(i==0){
+                ans = current.val;
+            } 
+            if(current.left != null ) q.addLast(current.left);
+            if(current.right != null) q.addLast(current.right);
             }
-            result.add(local);
+            
         }
-        return result.get(result.size()-1).get(0);
+        return ans;
     }
 }
