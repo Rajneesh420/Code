635dc45b9e2e92cdff83e9b8b3eac50c8f327320
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-09-19 13:06:15 +0530

    Time: 6 ms (72.32%) | Memory: 45.2 MB (68.59%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,30 +1,40 @@
 class Solution {
-    public boolean canFinish(int numC, int[][] pre) {
-        int[] in = new int[numC];
-        List<List<Integer>> graph = new LinkedList<>();
-        for(int i=0;i<numC;i++) graph.add(i,new ArrayList<>());
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>(numCourses);
+        Queue<Integer> leafs = new LinkedList<>();
+        int numProcessed = 0;
+        int[] indegree = new int[numCourses];
 
-        for(int[] p:pre){
-            in[p[0]]++;
-            graph.get(p[1]).add(p[0]);
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
         }
-        Deque<Integer> que = new LinkedList<>();
-        for(int i=0;i<numC;i++){
-            if(in[i]==0) que.addLast(i);
+
+        for (int[] prereq : prerequisites) {
+            adjList.get(prereq[1]).add(prereq[0]);
+
+            indegree[prereq[0]]++;
         }
-        int idx = 0;
-        int[] result = new int[numC];
-        while(!que.isEmpty()) {
-            int t = que.pollFirst();
-            result[idx++]=t;
-            for(int a : graph.get(t)){
-                in[a]--;
-                if(in[a]==0){
-                    que.addLast(a);
-                }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                leafs.offer(i);
             }
         }
-        if(idx == numC) return true;
-        return false;
+
+        while (!leafs.isEmpty()) {
+            int l = leafs.poll();
+
+            for (int v : adjList.get(l)) {
+                indegree[v]--;
+
+                if (indegree[v] == 0) {
+                    leafs.offer(v);
+                }
+            }
+
+            numProcessed++;
+        }
+
+        return numProcessed == numCourses;
     }
 }
