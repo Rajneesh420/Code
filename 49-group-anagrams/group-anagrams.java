1621163ce2550337cd991886591eb252ac83b658
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-11-09 10:09:13 +0530

    Time: 7 ms (63.13%) | Memory: 49.8 MB (7.08%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,16 +1,21 @@
-class Solution {
+import java.util.*;
+
+public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> res = new HashMap<>();
-
-        for(String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);
-            String key = new String(chars);  // Sorted string as the key
-
-            res.putIfAbsent(key, new ArrayList<>());
-            res.get(key).add(s);
+        if (strs == null || strs.length == 0) {
+            throw new IllegalArgumentException("Input array must have at least one element.");
         }
 
-        return new ArrayList<>(res.values());
+        Map<String, List<String>> dp = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);  // Sorted string as key
+
+            dp.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(dp.values());
     }
 }
