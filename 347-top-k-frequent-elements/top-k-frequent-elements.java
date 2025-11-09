eec2a3b42eb313d4edf3f03dbfe6662a257d08a4
Rajneesh Madhesiya <43504702+Rajneesh420@users.noreply.github.com>
2025-11-09 10:33:24 +0530

    Time: 14 ms (59.87%) | Memory: 47.7 MB (74.25%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,25 +1,24 @@
-class Solution {
+import java.util.*;
+
+public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       Map<Integer, Integer> freqMap = new HashMap<>();
+        Map<Integer, Integer> freqMap = new HashMap<>();
         for (int num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            heap.offer(new int[]{entry.getKey(), entry.getValue()});
-            
-            if (heap.size() > k) {
-                heap.poll();
-            }
-        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) pq.poll(); 
+        }
         int[] result = new int[k];
         int index = 0;
-        
-        while (!heap.isEmpty()) {
-            result[index++] = heap.poll()[0];
+        while (!pq.isEmpty()) {
+            result[index++] = pq.poll().getKey();
         }
-        
+
         return result;
     }
 }
